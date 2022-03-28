9023399496a6754804a9628d4ae00172015e4192
quarkusio <no-reply@quarkus.io>
2022-03-28 19:21:33 +0000

    Initial commit

---
+++ src/native-test/java/com/project/password/manager/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package com.project.password.manager;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
